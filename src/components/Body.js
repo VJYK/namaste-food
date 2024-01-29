33b5d3578fc38d1dcacb3d688fa04e58e8324321
VJYK <vijaymishravm99@gmail.com>
2024-01-30 00:13:02 +0530

    Api itegrate and added search feature

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,22 +1,54 @@
 import {restaurentList} from '../utils/mockData';
 import RestaurentCard from './RestaurentCard';
 
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
+import Shimmer from './Shimmer';
 
 const Body = () => {
-  const [listOfRestaurent,setListOfRestaurent]= useState(restaurentList);
-  console.log(listOfRestaurent)
-    return (
+  const [listOfRestaurent,setListOfRestaurent]= useState([]);
+  const [filteredRestaurent,setFilteredRestaurent]= useState([]);
+  const [searchText,setSearchText]=useState('');
+  useEffect(() => {
+   fetchData()
+  },[])
+
+  const fetchData =async ()=>{
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6237315&lng=77.2748902&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+    const json = await data.json()
+    console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+    //Optional Chaining
+    setListOfRestaurent(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    setFilteredRestaurent(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    console.log(listOfRestaurent)
+  }
+
+  //Conditional Rendering
+  return listOfRestaurent.length==0?<Shimmer/>: (
       <div className="body">
         <div className="search">
-          <button className="filter-btn" onClick={()=>{
-            let filterRestaurent = listOfRestaurent.filter(res=>res.info.avgRating>4);
-            setListOfRestaurent(filterRestaurent)
-          }
-          }>Top Rated Restaurents</button>
+          <div>
+            <input type='text' value={searchText} onChange={(e)=>setSearchText(e.target.value)}/>
+            <button onClick={()=>{const filteredRestaurent= listOfRestaurent.filter(res=> res.info.name.toLowerCase().includes(searchText.toLowerCase()))
+              setFilteredRestaurent(filteredRestaurent)
+            }}>Search</button>
+          </div>
+
+          <button
+            className="filter-btn"
+            onClick={() => {
+              let filterRestaurent = listOfRestaurent.filter(
+                (res) => res.info.avgRating > 4
+              );
+              setListOfRestaurent(filterRestaurent);
+            }}
+          >
+            Top Rated Restaurents
+          </button>
         </div>
         <div className="res-container">
-          {listOfRestaurent.map(restaurent=><RestaurentCard key={restaurent.info.id} resData={restaurent}/>)}
+          {filteredRestaurent.map((restaurent) => (
+            <RestaurentCard key={restaurent.info.id} resData={restaurent} />
+          ))}
         </div>
       </div>
     );
