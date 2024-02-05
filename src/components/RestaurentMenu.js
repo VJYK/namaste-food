cff0964bcfc5dca29287507e673b1277d3dce155
VJYK <vijaymishravm99@gmail.com>
2024-02-05 23:28:38 +0530

    Implemented custom hook RestaurantMenu

--- src/components/RestaurentMenu.js
+++ src/components/RestaurentMenu.js
@@ -1,32 +1,18 @@
-import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import {MENU_API} from '../utils/constant';
+import useRestaurantMenu from '../utils/useRestaurant';
 const RestaurentMenu= ()=>{
-    const {resId} = useParams()
-    const [resInfo,setResInfo] = useState(null);
-    useEffect(()=>{
-        fetchData()
-    },[])
-    
-    const fetchData = async ()=>{
-        const data = await fetch(MENU_API+resId)
-        const json = await data.json()
-        setResInfo(json.data)
-        console.log(json.data.cards)
-    }
+    const {resId} = useParams();
+    const resInfo = useRestaurantMenu(resId);
     if(resInfo == null){
         return <Shimmer/>
     } 
-        
-
     const {name,cuisines,costForTwoMessage} = resInfo?.cards[0]?.card?.card?.info;
     const {itemCards} = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR.cards[2].card.card;
     console.log(itemCards)
     return <>
         <h3>{name}</h3>
         <p>{costForTwoMessage} - {cuisines.join(', ')}</p>
-
         <ul>
             {itemCards.map(item=>
                 <li key={item.card.info.id}>{item.card.info.name}+ -  Rs. +{item.card.info.price/100}</li>
